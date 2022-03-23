f3611cd8881e05bbe4e7ab665fceebcbd6f0df74
DysprosiumDy <59086724+Dysprosium0626@users.noreply.github.com>
2022-03-23 22:03:10 +0800

    commit
    commit

---
+++ dashboard-portal/src/main/java/com/techtree/dashboardportal/DashboardPortalApplication.java
@@ -0,0 +1,13 @@
+package com.techtree.dashboardportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardPortalApplication.class, args);
+    }
+
+}
---
+++ dashboard-portal/src/test/java/com/techtree/dashboardportal/DashboardPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtree.dashboardportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
